f93977d5d4963c2631f68fc5ff9517ab2d9bb568
ayrton <ayrton.parkinson1@gmail.com>
2022-11-11 13:47:06 -0500

    Add comments

--- src/services/authHeader.js
+++ src/services/authHeader.js
@@ -1,3 +1,4 @@
+// Gets user authorization token from localStorage and returns correct Auth header
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
 
