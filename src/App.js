9c499c2dd63a12286c5e919b4816b907f0939f96
ayrton <ayrton.parkinson1@gmail.com>
2022-11-12 09:40:14 -0500

    Add AuthService and routes to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard';
+import Navbar from './components/navbar/Navbar';
+import Login from './components/Login';
+import HomePage from './components/HomePage';
+import AuthService from './services/AuthService';
+import './styles/App.css'
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    setCurrentUser(AuthService.getCurrentUser())
+  }, [])
+
   return (
-    <div></div>
+    <div className="App">
+      <Navbar currentUser={currentUser} />
+      <div className="App-main-content">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Login />} />
+          {currentUser ?
+            <Route path="/*" element={<Dashboard projects={[]} />} /> :
+            <Route path="/" element={<HomePage />} />
+          }
+        </Routes>
+      </div>
+    </div>
   );
 }
 
