9b8156b8cc59f1007f8bd6cdee732abcef5d81f1
ayrton <ayrton.parkinson1@gmail.com>
2022-11-12 09:37:10 -0500

    Add navbar links and logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import AuthService from '../../services/AuthService';
+import logoSVG from '../../assets/icons/logo.svg';
 import '../../styles/Nav.css'
 
 function Navbar() {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    setCurrentUser(AuthService.getCurrentUser());
+  }, [])
+
+  const logOut = () => {
+    AuthService.logout();
+    setCurrentUser(null);
+  }
+
   return (
     <nav className="Nav">
+      <Link to="/">
+        <img src={logoSVG} className="Nav-logo" alt="" />
+      </Link>
+      { currentUser ? 
+        <a href="/login" onClick={logOut}>Logout</a> :
+        <Link to="/login">Login</Link>
+      }
     </nav>
   );
 }
