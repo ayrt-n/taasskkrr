346f1ed0aae3f47215930788ce190c77b66e04ae
ayrton <ayrton.parkinson1@gmail.com>
2022-11-14 06:45:32 -0500

    Add register route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard';
 import Navbar from './components/navbar/Navbar';
 import Login from './components/Login';
+import Register from './components/Register';
 import HomePage from './components/HomePage';
 import AuthService from './services/AuthService';
 import './styles/App.css'
@@ -20,7 +21,7 @@ function App() {
       <div className="App-main-content">
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           {currentUser ?
             <Route path="/*" element={<Dashboard projects={[]} />} /> :
             <Route path="/" element={<HomePage />} />
