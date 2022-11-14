022e9408ec002a57923c81e29fffc84df9ec6ea9
ayrton <ayrton.parkinson1@gmail.com>
2022-11-14 06:39:19 -0500

    Add Alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Alert.css';
+
+function Alert({ type, message, details }) {
+  return(
+    <div className={`Alert ${type}`}>
+      <div className="Alert-message">{message}</div>
+      {details &&
+        <ul className="Alert-details">
+          {details.map((detail) => {
+            return(
+              <li>
+                {detail}
+              </li>
+            );
+          })}
+        </ul>
+      }
+    </div>
+  );
+}
+
+export default Alert;
