c8e80b6dd17807b93f8240d7b7b163e737433d51
ayrton <ayrton.parkinson1@gmail.com>
2022-11-15 08:10:45 -0500

    Refactor form to use Formik

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,77 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
 import AuthService from '../services/AuthService';
 import { withRouter } from './helpers/withRouter';
-import '../styles/Login.css'
+import Alert from './Alert';
+import '../styles/Form.css'
 
 function Login(props) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState([]);
 
-  const onEmailChange = (e) => {
-    setEmail(e.target.value);
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = 'Required';
+    }
+
+    if (!values.password) {
+      errors.password = 'Required';
+    }
+
+    return errors;
   };
 
-  const onPasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    AuthService.login(email, password)
+  const handleLogin = (values, { setSubmitting }) => {
+    AuthService.login(values.email, values.password)
     .then((data) => {
       if (!data.error) {
         props.router.navigate('/inbox');
         window.location.reload();
       } else {
-        console.log(data);
+        console.log(data.error);
+        setErrorMessage([data.error]);
       }
+      setSubmitting(false);
     })
-  }
+  };
 
   return (
-    <div className="Login">
-      <form className="Form" onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" name="user[email]" value={email} onChange={onEmailChange}></input>
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={password} onChange={onPasswordChange}></input>
-        </div>
-        <div>
-          <button>Login</button>
-        </div>
-      </form>
+    <div className="Form-container">
+      {errorMessage.length > 0 && <Alert type="danger" message="Error attempting to sign in:" details={errorMessage} />}
+      <Formik
+        initialValues={{email: '', password: ''}}
+        validate={validate}
+        onSubmit={handleLogin}
+      >
+        {formik => (
+          <form onSubmit={formik.handleSubmit}>
+             <div className="field">
+              <label htmlFor="email" className="label">Email</label>
+              <input type="text" id="email" className="input" {...formik.getFieldProps("email")} />
+              {formik.touched.email && formik.errors.email ? (
+                <div className="Form-error-message">{formik.errors.email}</div>
+              ) : null}
+            </div>
+
+            <div className="field">
+              <label htmlFor="password" className="label">Password</label>
+              <input type="password" id="password" className="input" {...formik.getFieldProps("password")} />
+              {formik.touched.password && formik.errors.password ? (
+                <div className="Form-error-message">{formik.errors.password}</div>
+              ) : null}
+            </div>
+
+            <div className="control">
+              <button className="button full" type="submit">Log in</button>
+            </div>
+          </form>
+        )}
+      </Formik>
+      <div style={{'textAlign': 'center'}}>
+        Already have an account? <Link to="/login">Log in</Link>
+      </div>
     </div>
   );
 }
