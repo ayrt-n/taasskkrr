e0b7cc406e578c95ac7024dd8c60ac01e0c0438f
ayrton <ayrton.parkinson1@gmail.com>
2022-11-15 07:22:21 -0500

    Refactor form to use Formik

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,63 +1,92 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
 import AuthService from '../services/AuthService';
 import { withRouter } from './helpers/withRouter';
 import Alert from './Alert';
 import '../styles/Form.css'
 
 function Register(props) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState('');
-  const [errors, setErrors] = useState([]);
+  const [errorMessage, setErrorMessage] = useState([]);
 
-  const onEmailChange = (e) => {
-    setEmail(e.target.value);
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password < 6) {
+      errors.password = 'Must be 6 characters or more';
+    }
+
+    if (!values.passwordConfirmation) {
+      errors.passwordConfirmation = 'Required';
+    } else if (values.password !== values.passwordConfirmation) {
+      errors.passwordConfirmation = 'Must match password';
+    }
+
+    return errors;
   };
 
-  const onPasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const onPasswordConfirmationChange = (e) => {
-    setPasswordConfirmation(e.target.value);
-  };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    AuthService.register(email, password, passwordConfirmation)
+  const handleRegister = (values, { setSubmitting }) => {
+    AuthService.register(values.email, values.password, values.passwordConfirmation)
     .then((data) => {
       if (!data.error) {
         props.router.navigate('/login');
         // window.location.reload();
       } else {
         console.log(data.error);
-        setErrors(data.error.details)
+        setErrorMessage(data.error.details)
       }
+      setSubmitting(false);
     })
   }
 
   return (
     <div className="Form-container">
-      {errors.length > 0 && <Alert type="danger" message="One or more errors prevented us from creating your account:" details={errors} />}
-      <form className="Form" onSubmit={handleLogin} autoComplete="new-password">
-        <div className="field">
-          <label htmlFor="email" className="label">Email</label>
-          <input autoComplete="new-password" type="text" name="user[email]" id="email" value={email} onChange={onEmailChange} className="input" />
-        </div>
-        <div className="field">
-          <label htmlFor="password" className="label">Password</label>
-          <input autoComplete="new-password" type="password" name="password" id="password" value={password} onChange={onPasswordChange} className="input" />
-        </div>
-        <div className="field">
-          <label htmlFor="password_confirmation" className="label">Confirm Password</label>
-          <input autoComplete="off" type="password" name="password_confirmation" id="password_confirmation" value={passwordConfirmation} onChange={onPasswordConfirmationChange} className="input" />
-        </div>
-        <div className="control">
-          <button className="button full">Sign up</button>
-        </div>
-      </form>
+      {errorMessage.length > 0 && <Alert type="danger" message="One or more errors prevented us from creating your account:" details={errorMessage} />}
+      <Formik
+        initialValues={{email: '', password: '', passwordConfirmation: ''}}
+        validate={validate}
+        onSubmit={handleRegister}
+      >
+        {formik => (
+          <form onSubmit={formik.handleSubmit}>
+             <div className="field">
+              <label htmlFor="email" className="label">Email</label>
+              <input type="text" id="email" className="input" {...formik.getFieldProps("email")} />
+              {formik.touched.email && formik.errors.email ? (
+                <div className="Form-error-message">{formik.errors.email}</div>
+              ) : null}
+            </div>
+
+            <div className="field">
+              <label htmlFor="password" className="label">Password</label>
+              <input type="password" id="password" className="input" {...formik.getFieldProps("password")} />
+              {formik.touched.password && formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+              ) : null}
+            </div>
+
+            <div className="field">
+              <label htmlFor="passwordConfirmation" className="label">Confirm Password</label>
+              <input type="password" id="passwordConfirmation" className="input" {...formik.getFieldProps("passwordConfirmation")} />
+              {formik.touched.passwordConfirmation && formik.errors.passwordConfirmation ? (
+                <div>{formik.errors.passwordConfirmation}</div>
+              ) : null}
+            </div>
+
+            <div className="control">
+              <button className="button full" type="submit">Sign up</button>
+            </div>
+          </form>
+        )}
+      </Formik>
       <div style={{'textAlign': 'center'}}>
         Already have an account? <Link to="/login">Log in</Link>
       </div>
