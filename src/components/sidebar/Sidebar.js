a27d552195299b09a44d63ffe3d98303153cbf1f
ayrton <ayrton.parkinson1@gmail.com>
2022-11-17 07:59:56 -0500

    Add useEffect to fetch projects from api

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import '../../styles/Sidebar.css'
 import SidebarItem from './SidebarItem'
 import SidebarCollapsableList from './SidebarCollapsableList';
+import UserService from '../../services/UserService';
 
-function Sidebar({ projects }) {
+function Sidebar() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    UserService.getUserProjects().then((userProjects) => {
+      setProjects(userProjects)
+    });
+  }, []);
+
   return (
     <div className="Sidebar">
       <SidebarItem icon="inbox.svg" title="Inbox" action="/inbox" />
