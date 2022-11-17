dda7f67f069636c5557c9db795d99db038c9e50c
ayrton <ayrton.parkinson1@gmail.com>
2022-11-17 07:42:43 -0500

    Add getUserProjects to UserServer

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,24 @@
 import authHeader from "./authHeader";
 
-const API_URL = 'http://localhost:3001/api/v1/tasks/'
+const API_URL = 'http://localhost:3001/api/v1'
 
 class UserService {
+  getUserProjects() {
+    return fetch(`${API_URL}/projects`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authHeader()
+      }
+    })
+    .then(response => {
+      return response.json();
+    });
+  }
+
   getUserTasks() {
-    return fetch(API_URL, {
+    return fetch(`${API_URL}tasks`, {
       method: 'GET',
       mode: 'cors',
       headers: {
