0c41a4df7e0466b777a76a30008ef7a87aac12a0
ayrton <ayrton.parkinson1@gmail.com>
2022-11-19 13:20:56 -0500

    Split out Task detail modal view from Task modal

---
+++ src/components/tasks/TaskDetails.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import closeIcon from '../../assets/icons/close.svg';
+import Priority from './Priority';
+import Button from '../form/Button';
+
+function TaskDetails({ task, closeModal, editTask }) {
+  return (
+    <>
+      <div className="Modal-header-container">
+        <h2>{task.title}</h2>
+        <button className="Close-modal-button" onClick={closeModal}>
+          <img src={closeIcon} alt="" />
+        </button>
+      </div>
+      <div className="Modal-content-container">
+        <div className="field">
+          <p className="label">Description</p>
+          <p>{task.description}</p>
+        </div>
+        <div className="field">
+          <p className="label">Priority</p>
+          <Priority priorityEnum={task.priority} />
+        </div>
+        <div className="field">
+          <p className="label">Due Date</p>
+          <p>{task.due_date}</p>
+        </div>
+        <Button label="Edit Task" primary onClick={editTask}/>
+      </div>
+    </>
+  );
+}
+
+export default TaskDetails;
