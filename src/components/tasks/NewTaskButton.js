5c6aa0a78cea8c3bf0d3392e6c128b8ee04d1968
ayrton <ayrton.parkinson1@gmail.com>
2022-11-22 07:47:59 -0500

    Add new task modal to button click

--- src/components/tasks/NewTaskButton.js
+++ src/components/tasks/NewTaskButton.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NewTaskModal from './NewTaskModal';
 import '../../styles/Tasks.css';
 
-function NewTaskButton({ handleClick }) {
+function NewTaskButton({ projectId, sectionId, afterSubmit }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
-    <button className="New-task-button" onClick={handleClick}>
-      <svg viewBox="0 0 24 24">
-        <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
-      </svg>
-      Add Task
-    </button>
+    <div>
+      <NewTaskModal
+        task={{}}
+        projectId={projectId}
+        sectionId={sectionId}
+        afterSubmit={afterSubmit}
+        isOpen={modalIsOpen}
+        closeModal={() => setModalIsOpen(false)}
+      />
+      <button className="New-task-button" onClick={() => setModalIsOpen(true)}>
+        <svg viewBox="0 0 24 24">
+          <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+        </svg>
+        Add Task
+      </button>
+    </div>
   );
 }
 
