d7d9e8835900e9e320a0dfbf06d55ff4366c9790
ayrton <ayrton.parkinson1@gmail.com>
2022-11-25 14:10:20 -0500

    Create date input component

---
+++ src/components/form/DateInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useField } from 'formik';
+import '../../styles/Form.css';
+
+function DateInput({ label, ...props }) {
+  const [field, meta] = useField(props);
+
+  return (
+    <div className="field">
+      <label htmlFor={props.id || props.name} className="label">{label}</label>
+      <div className="control">
+        <div className="date">
+          <input type="date" {...field} {...props} />
+        </div>
+      </div>
+      {meta.touched && meta.error ? (
+        <div className="Form-error-message">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+}
+
+export default DateInput;
