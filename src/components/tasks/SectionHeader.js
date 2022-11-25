676e594d42a98193337a172ebf47b5a7fcc440ce
ayrton <ayrton.parkinson1@gmail.com>
2022-11-25 07:19:06 -0500

    Add ability to open edit modal for project/section

--- src/components/tasks/SectionHeader.js
+++ src/components/tasks/SectionHeader.js
@@ -3,15 +3,31 @@ import DropdownMenu from '../DropdownMenu';
 import '../../styles/DropdownMenu.css'
 
 
-function SectionHeader({ title, headingLevel, name }) {
+function SectionHeader({ title, headingLevel, name, projectId, sectionId, openModal, handleEdit, handleDelete }) {
   const Title = headingLevel;
+
+  const openEditModal = () => {
+    if (sectionId) {
+      openModal('editSection', {projectId, section: { title, id: sectionId }, callback: handleEdit})
+    } else {
+      openModal('editProject', {project: { title, id: projectId }, callback: handleEdit})
+    }
+  };
+
+  const openDeleteModal = () => {
+    
+  };
 
   return (
     <div className="Section-header">
       <Title>{title}</Title>
       <DropdownMenu>
-        <li className="Dropdown-item"><button>Edit {name}</button></li>
-        <li className="Dropdown-item"><button>Delete {name}</button></li>
+        <li className="Dropdown-item">
+          <button onClick={openEditModal}>Edit {name}</button>
+        </li>
+        <li className="Dropdown-item">
+          <button onClick={openDeleteModal}>Delete {name}</button>
+        </li>
       </DropdownMenu>
     </div>
   );
