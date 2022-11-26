0ec32b16ca1564ff717f335e823b414d95c438c6
ayrton <ayrton.parkinson1@gmail.com>
2022-11-26 12:25:26 -0500

    Add API call to get and display all current tasks

--- src/components/tasks/Today.js
+++ src/components/tasks/Today.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import UserService from '../../services/UserService';
+import Task from './Task';
+import SectionHeader from './SectionHeader';
 import '../../styles/Tasks.css';
 
 function Today() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    UserService.getTodayTasks().then((userTasks) => {
+      setTasks(userTasks.tasks);
+    });
+  }, []);
+
   return (
-    <div className="Tasks-container">
-      <h1>Today</h1>
+    <div className="Tasks">
+      <div className="Tasks-container">
+        <SectionHeader
+          title="Today"
+          headingLevel="h1"
+          inbox={true}
+        />
+        {tasks.map((task) => {
+          return (
+            <Task
+              key={task.id}
+              task={task}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
