357e1756acfe61576cb0ffae4c30ac3434bb7160
ayrton <ayrton.parkinson1@gmail.com>
2022-11-26 11:42:37 -0500

    Add API call to get and display all upcoming tasks

--- src/components/tasks/Upcoming.js
+++ src/components/tasks/Upcoming.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import UserService from '../../services/UserService';
+import parseISO from 'date-fns/parseISO';
+import format from 'date-fns/format';
+import Task from './Task';
+import SectionHeader from './SectionHeader';
 import '../../styles/Tasks.css';
 
 function Upcoming() {
+  const [loading, setLoading] = useState(true);
+  const [upcoming, setUpcoming] = useState([])
+
+  useEffect(() => {
+    UserService.getUpcomingTasks().then((userTasks) => {
+      console.log(userTasks.tasks);
+      setUpcoming(userTasks.tasks);
+    })
+    .then(() => {
+      setLoading(false);
+      console.log(upcoming);
+    });
+  }, []);
+
   return (
-    <div className="Tasks-container">
-      <h1>Upcoming</h1>
+    // If loading show null
+    loading ?
+    null :
+    // Otherwise, render tasks in order of upcoming date
+    <div className="Upcoming">
+      <SectionHeader
+        title="Upcoming"
+        headingLevel="h1"
+        inbox={true}
+      />
+      {Object.keys(upcoming).sort().map((date) => {
+        if (date === "") {
+          return null;
+        } else {
+          return (
+            <div className="Tasks-container" key={date}>
+              <SectionHeader
+                title={format(parseISO(date), 'PPP')}
+                headingLevel="h2"
+                inbox={true}
+              />
+              {upcoming[date].map((task) => (
+                <Task
+                  key={task.id}
+                  task={task}
+                /> 
+              ))}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
