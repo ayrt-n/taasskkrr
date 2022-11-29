cb624fad3b1c1363efc2c386f80a4c121515130e
ayrton <ayrton.parkinson1@gmail.com>
2022-11-28 19:38:48 -0500

    Remove unnecessary modal styles

--- src/components/tasks/TaskModal.js
+++ src/components/tasks/TaskModal.js
@@ -7,20 +7,6 @@ import '../../styles/Form.css';
 function TaskModal({ task, sectionId, closeModal, afterSubmit }) {
   const [editMode, setEditMode] = useState(false);
 
-  const modalStyles = {
-    overlay: {
-      backgroundColor: 'rgba(0, 0, 0, 0.4)',
-      zIndex: '40'
-    },
-    content: {
-      border: '2px solid var(--bgLayerColor)',
-      background: 'var(--bgColor)',
-      margin: '0 auto',
-      maxHeight: 'min-content',
-      maxWidth: '900px'
-    }
-  };
-
   return (
     <>
       {editMode ?
