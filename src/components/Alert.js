08ae309bfd7af9c80e5931fbeb885403da879cab
ayrton <ayrton.parkinson1@gmail.com>
2022-12-06 17:54:04 -0500

    Add option to provide plain text to Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/Alert.css';
 
-function Alert({ type, message, details }) {
+function Alert({ type, message, body, details }) {
   return(
     <div className={`Alert ${type}`}>
       <div className="Alert-message">{message}</div>
+      {body ? <div>{body}</div> : null}
       {details &&
         <ul className="Alert-details">
           {details.map((detail) => {
