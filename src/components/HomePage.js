784d08126bc9c537271b98a5c212fa4525ade2ac
ayrton <ayrton.parkinson1@gmail.com>
2022-12-06 18:56:25 -0500

    Add copy text and hero

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import '../styles/Landing.css';
+import heroMain from '../assets/images/hero-main.png';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return(
-    <div>Home page - LOGIN IF YOU WANT TO DO ANYTHING</div>
+    <div className="Landing">
+      <div className="Landing-hero-text">
+        <h1>Tired of losing track? Organize your work and life, finally.</h1>
+        <h2>Become focused, organized, and calm with Taasskkrr. What are you waiting for?</h2>
+        <div>
+          <Link to="/register" className="button is-primary">Get Started!</Link>
+        </div>
+      </div>
+      <div className="Landing-hero-container">
+        <img src={heroMain} alt="" className="Landing-hero-main" />
+      </div>
+    </div>
   );
 }
 
