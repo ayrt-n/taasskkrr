cff7072eb13f08cf06f7dd533ed2cc194fe1fad3
ayrton <ayrton.parkinson1@gmail.com>
2022-12-07 08:13:01 -0500

    Add data-testid for identifying Alert components

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,14 +3,14 @@ import '../styles/Alert.css';
 
 function Alert({ type, message, body, details }) {
   return(
-    <div className={`Alert ${type}`}>
-      <div className="Alert-message">{message}</div>
-      {body ? <div>{body}</div> : null}
+    <div className={`Alert ${type}`} data-testid="alert">
+      <div className="Alert-message" data-testid="alert-message">{message}</div>
+      {body ? <div data-testid="alert-body">{body}</div> : null}
       {details &&
         <ul className="Alert-details">
           {details.map((detail) => {
             return(
-              <li key={detail}>
+              <li key={detail} data-testid="alert-detail">
                 {detail}
               </li>
             );
