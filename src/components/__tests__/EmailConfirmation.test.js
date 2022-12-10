fd3be6e0b1d13dac1f781f3df352a4732ce0d2a2
ayrton <ayrton.parkinson1@gmail.com>
2022-12-10 12:02:55 -0500

    Refactor tests to use waitFor

--- src/components/__tests__/EmailConfirmation.test.js
+++ src/components/__tests__/EmailConfirmation.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import EmailConfirmation from '../EmailConfirmation';
 import { confirmEmail } from '../../services/accountServices';
 
-// Mock implementation of confirmEmail method. If provided with 'goodToken'
-// it will return a promise which resolves with no error key
+// Set up mock for confirmEmail
 jest.mock('../../services/accountServices', () => ({
   ...jest.requireActual('../../services/accountServices'),
   confirmEmail: jest.fn()
@@ -25,20 +24,22 @@ describe('EmailConfirmation component', () => {
       const successfulResponse = { response: 'success' };
       confirmEmail.mockResolvedValue(successfulResponse);
 
-      await render(
+      render(
         <MemoryRouter initialEntries={['?confirmation_token=goodToken']}>
           <EmailConfirmation />
         </MemoryRouter>
       );
 
-      expect(mockUseNavigate).toHaveBeenCalledWith(
-        "/login",
-        { "state":
-          {
-            "body": "Log in and get started!", "message": "Email confirmed!", "type": "success"
+      await waitFor(() => {
+        expect(mockUseNavigate).toHaveBeenCalledWith(
+          "/login",
+          { "state":
+            {
+              "body": "Log in and get started!", "message": "Email confirmed!", "type": "success"
+            }
           }
-        }
-      );
+        );
+      });
     });
   });
 
@@ -47,20 +48,22 @@ describe('EmailConfirmation component', () => {
       const failedResponse = { error: { details: 'failure' } };
       confirmEmail.mockResolvedValue(failedResponse);
 
-      await render(
+      render(
         <MemoryRouter initialEntries={['?confirmation_token=badToken']}>
           <EmailConfirmation />
         </MemoryRouter>
       );
 
-      expect(mockUseNavigate).toHaveBeenCalledWith(
-        "/login",
-        { "state":
-          {
-            "details": "failure", "message": "Unable to confirm email:", "type": "danger"
+      await waitFor(() => {
+        expect(mockUseNavigate).toHaveBeenCalledWith(
+          "/login",
+          { "state":
+            {
+              "details": "failure", "message": "Unable to confirm email:", "type": "danger"
+            }
           }
-        }
-      );
+        );
+      });
     });
   });
 });
