1537a2bca926b51a39a745b1ff2140e5630e2e9e
ayrton <ayrton.parkinson1@gmail.com>
2022-12-11 13:11:05 -0500

    Split routes into public and private routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './components/navbar/Navbar';
 import Login from './components/Login';
 import Register from './components/Register';
 import HomePage from './components/HomePage';
+import PrivateRoute from './components/common/PrivateRoute';
+import PublicRoute from './components/common/PublicRoute';
 import { getCurrentUser, logout } from './services/authService';
 import EmailConfirmation from './components/EmailConfirmation';
 import EmailConfirmationForm from './components/EmailConfirmationForm';
@@ -47,19 +49,73 @@ function App() {
 
       <div className="App-main-content">
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {currentUser ?
-            <Route
-              path="/*"
-              element={<Dashboard sidebarOpen={sidebarOpen} closeSidebar={() => {setSidebarOpen(false)}} />}
-            /> :
-            <Route path="/" element={<HomePage />} />
-          }
-          <Route path="/api/v1/confirmation" element={<EmailConfirmation />} />
-          <Route path="/confirmations" element={<EmailConfirmationForm />} />
-          <Route path="api/v1/password/edit" element={<ResetPasswordForm />} />
-          <Route path="/forgot_password" element={<ForgotPasswordForm />} />
+          {/* Public Routes */}
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            } 
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <HomePage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/api/v1/confirmation"
+            element={
+              <PublicRoute>
+                <EmailConfirmation />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/confirmations"
+            element={
+              <PublicRoute>
+                <EmailConfirmationForm />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="api/v1/password/edit"
+            element={
+              <PublicRoute>
+                <ResetPasswordForm />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/forgot_password"
+            element={
+              <PublicRoute>
+                <ForgotPasswordForm />
+              </PublicRoute>
+            }
+          />
+          
+          {/* Private Routes */}
+          <Route
+            path="/app/*"
+            element={
+              <PrivateRoute>
+                <Dashboard sidebarOpen={sidebarOpen} closeSidebar={() => {setSidebarOpen(false)}} />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
       
