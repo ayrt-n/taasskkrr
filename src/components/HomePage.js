5e8bc746084ef86386cd6776ad0a0cc6972f1add
ayrton <ayrton.parkinson1@gmail.com>
2022-12-13 07:31:50 -0500

    Add hero text and image examples to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import '../styles/Landing.css';
+import logo from '../assets/icons/logo.svg';
 import heroMain from '../assets/images/hero-main.png';
+import smallExample1 from '../assets/images/mobile-example1.png';
 import { Link } from 'react-router-dom';
 
 function HomePage() {
   return(
     <div className="Landing">
+      {/* Main Section with Hero Text and Sign up link */}
       <div className="Landing-hero-text">
-        <h1>Tired of losing track? Organize your work and life, finally.</h1>
-        <h2>Become focused, organized, and calm with Taasskkrr. What are you waiting for?</h2>
+        <h1>Never miss a deadline again.</h1>
+        <h2>Stay organized, focused, and get things done with Taasskkrr. The ultimate to-do list application.</h2>
         <div>
           <Link to="/register" className="button is-primary">Get Started!</Link>
         </div>
+        <div>
+          <img src={logo} alt="" className="Landing-logo"/>
+        </div>
       </div>
-      <div className="Landing-hero-container">
-        <img src={heroMain} alt="" className="Landing-hero-main" />
+
+      {/* Secondary section with example images */}
+      <div className="Landing-secondary-section">
+        <div className="Landing-hero-container">
+          <img src={heroMain} alt="screenshot of the main dashboard for Taasskkrr" className="Landing-hero-main" />
+        </div>
+        <div className="Landing-two-grid">
+          <div className="Landing-pitch-container">
+            <p className="Landing-pitch-whisper">Simplify your life</p>
+            <h3>
+              <span>Add your tasks.</span>
+              <span>Organize your life.</span>
+              <span>Achieve more every day.</span>
+            </h3>
+            <p className="Landing-pitch">
+              Break bigger tasks down and keep them organized by creating separate projects. Whether you're working on a big project at work, planning a party, or just trying to stay on top of your daily tasks, our app has you covered.
+            </p>
+          </div>
+          <img src={smallExample1}
+            alt="screenshot of mobile view of Taasskkrr app viewing more details of a task"
+            className="Landing-mobile-screen"
+          />
+        </div>
       </div>
     </div>
   );
