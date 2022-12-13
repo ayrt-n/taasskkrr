b730a66154bbbb9074a6419f1dc35d99bc049dc8
ayrton <ayrton.parkinson1@gmail.com>
2022-12-13 16:22:14 -0500

    Add wavy breaker to pahe

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -20,6 +20,14 @@ function HomePage() {
         </div>
       </div>
 
+      <section>
+        <div class="Landing-wave">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+      </section>
+
       {/* Secondary section with example images */}
       <div className="Landing-secondary-section">
         <div className="Landing-hero-container">
@@ -29,8 +37,8 @@ function HomePage() {
           <div className="Landing-pitch-container">
             <p className="Landing-pitch-whisper">Simplify your life</p>
             <h3>
-              <span>Create a project.</span>         
-              <span>Add your tasks.</span>              
+              <span>Create your project.</span>
+              <span>Add your tasks.</span>
               <span>Conquer the day.</span>
             </h3>
             <p className="Landing-pitch">
