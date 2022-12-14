e0207a854f5bad3bb9407c4e1ed7d91f5f0fd9a7
ayrton <ayrton.parkinson1@gmail.com>
2022-12-14 09:44:43 -0500

    Add ability to edit/delete tasks

--- src/components/tasks/Upcoming.js
+++ src/components/tasks/Upcoming.js
@@ -12,13 +12,24 @@ function Upcoming({ openModal }) {
   useEffect(() => {
     document.title = "Upcoming"
     UserService.getUserTasks().then((userTasks) => {
-      console.log(userTasks.tasks);
       setTasks(userTasks.tasks.filter(task => task.due_date));
     })
     .then(() => {
       setLoading(false);
     });
   }, []);
+
+  const updateTask = (updatedTask) => {
+    setTasks((prevTasks) => {
+      return prevTasks.map((task) => (
+        task.id === updatedTask.id ? updatedTask : task
+      ));
+    })
+  };
+
+  const deleteTask = (deletedTask) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== deletedTask.id));
+  };
 
   const sortedTasks = useMemo(() => (
     tasks.sort((a, b) => new Date(a.due_date) - new Date(b.due_date))
@@ -33,16 +44,16 @@ function Upcoming({ openModal }) {
       const formattedDate = taskDueDate < Date.now() ? 'Overdue' : format(parseISO(task.due_date), 'PPP')
 
       if (dueDate === formattedDate) {
-        list.push(<Task key={task.id} task={task} openModal={openModal} />);
+        list.push(<Task key={task.id} task={task} handleUpdate={updateTask} handleDelete={deleteTask} openModal={openModal} />);
       } else {
         list.push(<div className="Section-header"><h2>{formattedDate}</h2></div>)
-        list.push(<Task key={task.id} task={task} openModal={openModal} />);
+        list.push(<Task key={task.id} task={task} handleUpdate={updateTask} handleDelete={deleteTask} openModal={openModal} />);
         dueDate = formattedDate
       }
     })
 
     return list;
-  }, [sortedTasks]);
+  }, [sortedTasks, openModal]);
 
   return (
     // If loading show null
