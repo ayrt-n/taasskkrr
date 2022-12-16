06bb19214547faa49534b960bd97fbca0011d4d8
ayrton <ayrton.parkinson1@gmail.com>
2022-12-16 09:43:20 -0500

    Add constants to export development or production vars based on env

---
+++ src/services/constants.js
@@ -0,0 +1,13 @@
+const prod = {
+  url: {
+   API_URL: 'https://rocky-lowlands-45874.herokuapp.com/api/v1',
+  }
+};
+
+const dev = {
+  url: {
+    API_URL: 'http://localhost:3000/api/v1'
+  }
+};
+
+export const config = process.env.NODE_ENV === 'development' ? dev : prod;
