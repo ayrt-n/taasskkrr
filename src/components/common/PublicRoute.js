2d74bd68901b8a000ee6c9a35577b8f4c931a4f6
ayrton <ayrton.parkinson1@gmail.com>
2023-01-10 07:28:25 -0500

    Add pingService to public routes

--- src/components/common/PublicRoute.js
+++ src/components/common/PublicRoute.js
@@ -1,8 +1,12 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { pingService } from '../../services/pingService';
 import AuthContext from '../../contexts/authentication/AuthContext';
 
 function PublicRoute({ children }) {
+  // Ping server to wake up heroku
+  pingService();
+
   const { currentUser } = useContext(AuthContext);
   return currentUser ? <Navigate to="/app" /> : children
 }
