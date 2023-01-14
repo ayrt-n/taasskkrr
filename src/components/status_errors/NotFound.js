12e36e2d11214e92974576aa838ff62189965fea
ayrton <ayrton.parkinson1@gmail.com>
2023-01-14 09:17:18 -0500

    Create 404 not found component

---
+++ src/components/status_errors/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Component for HTTP Status 404 - Not Found
+function NotFound() {
+  return (
+    <div>
+      404???
+    </div>
+  );
+}
+
+export default NotFound;
