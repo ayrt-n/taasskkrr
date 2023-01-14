8d460d6f9132cab3e07265c19388aa898673ba50
ayrton <ayrton.parkinson1@gmail.com>
2023-01-14 07:51:36 -0500

    Add explanatory comments

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,9 +1,11 @@
+// Production API URL 
 const prod = {
   url: {
    API_URL: 'https://rocky-lowlands-45874.herokuapp.com/api/v1',
   }
 };
 
+// Development API URL
 const dev = {
   url: {
     API_URL: 'http://localhost:3001/api/v1'
